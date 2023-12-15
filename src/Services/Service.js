162e5cfd739b72a515db2aff7de4962c4876d457
Heitor Loguércio Perrotta <54395989870@info.local>
2023-12-15 17:10:37 -0300

    final semestre

--- src/Services/Service.js
+++ src/Services/Service.js
@@ -42,7 +42,7 @@ export const loginResource = '/Login';
 
 //const apiPort = '5000';
 //const localApiUri = `http://localhost:${apiPort}/api`;
-const externallApiUri = ``;
+const externallApiUri = `https://galinhadaangola.azurewebsites.net/api`;
 // const externalApiUri = null;
 
 const api = axios.create({
